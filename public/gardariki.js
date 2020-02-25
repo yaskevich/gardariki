989a234d1acf642fdc87d3ee2229f1a8e877dfc4
yaskevich <alyaxey.yaskevich@gmail.com>
2020-02-25 23:13:23 +0300

    SVG marker layer removed

--- public/gardariki.js
+++ public/gardariki.js
@@ -1,335 +1,141 @@
-var defLayerIndex = 10;
-var osm_attr = 'Map data <a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia maps</a> &copy; <a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a>';
-var wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 1} );
-var datamap = L.tileLayer('https://datamap.by/bright/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 1} );
-var natmap = L.tileLayer.wms("http://basemap.nationalmap.gov/ArcGIS/services/USGSImageryTopo/MapServer/WMSServer", { layers: "0", format: "image/png", transparent: false, attribution: "USGS" });
+$(function() {
+	var pois = {};
+	var defLayerIndex = 10;
+	var osm_attr = 'Map data <a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia maps</a> &copy; <a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a>';
+	var wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 1} );
+	var datamap = L.tileLayer('https://datamap.by/bright/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 1} );
+	var natmap = L.tileLayer.wms("http://basemap.nationalmap.gov/ArcGIS/services/USGSImageryTopo/MapServer/WMSServer", { layers: "0", format: "image/png", transparent: false, attribution: "USGS" });
 
-function getColor(d) {
-    return d > 50 ? '#800026' :
-           d > 40  ? '#BD0026' :
-           d > 30  ? '#E31A1C' :
-           d > 20  ? '#FC4E2A' :
-           d > 15   ? '#FD8D3C' :
-           d > 10   ? '#FEB24C' :
-           d > 5   ? '#FED976' :
-                      '#FFEDA0';
-}
-
-function style(feature) {
-    return {
-        fillColor: "red",
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-};
-var redMarker = L.VectorMarkers.icon({
-	//icon: 'exclamation-triangle',
-	icon: 'landmark',
-	markerColor:"white",
-	iconColor: "black",
-	extraClasses: 'fas',
-	prefix: 'fa'
-	// spin: true,
-  });
-
-// init:
-
-function popUp(f,l){
-	var desc = '';
-	var p = f.properties;
-    // var out = [];
-    // if (f.properties){
-        // for(key in f.properties){
-            // out.push(key+": "+f.properties[key]);
-        // }
-        // l.bindPopup(out.join("<br />"));
-    // }
-	desc+="<b>"+p.title+"</b><br/>Тип: "+p.type+"<br/>Дата установки: "+p.open+"</br>Адрес: "+p.addr;
-	if (p.fulltext){
-		desc+="<div style=\"border: 2px solid black;padding: 5px;margin-top: 5px;\"><i>"+p.fulltext+"</i></div>";
-	}
-	if (p.init){
-		desc+="</br>Установка инициирована: " + p.init;
-	}
-	l.bindPopup(desc);
-}
-
-var baseLayers2 = {			
-
-	"Datamap":  datamap,
-	"OSM":  L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 1} ),
-	"OSM.DE":  L.tileLayer("https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png", { maxZoom: 19, minZoom: 4, opacity: 1} ),
-	"AWMC":  L.tileLayer("https://a.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png", { maxZoom: 19, minZoom: 6, opacity: 1} ),
-	//positron
-	"CartoDB":  L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", { maxZoom: 19, minZoom: 6, opacity: 1, "attribution":
-      "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://cartodb.com/attributions'>CartoDB</a>",
-    "subdomains": "abcd"} ),
-	"OSM (no lbl)":  L.tileLayer('https://tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 1} ),
-	"Watercolor":  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', { maxZoom: 19, minZoom: 6, opacity: 1} ),
-	"River":  L.tileLayer('https://{s}.tile.openstreetmap.fr/openriverboatmap/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 1} ),
-	//'',
-	// ,
-	"Wikimedia": wikimedia,
-	"USGS": natmap,
-	"ESRI Satellite": L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'	, { attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, 	IGP, UPR-EGP, and the GIS User Community'}),
-	"Google Satellite" : L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-		maxZoom: 20,
-		subdomains:['mt0','mt1','mt2','mt3'],
-		attribution: "Google Maps"
-	})	
-};
-
-// var vkl = new L.featureGroup({interactive: false});
-// vkl.setStyle({className: 'vkl'});
-// vkl.bringToBack();
-
-
-// var testlayer = L.featureGroup().addTo(map);  
-// var testlayer = L.featureGroup({attribution: "Gardariki", className: 'test', pane: 'cities'})
-var testlayer = L.featureGroup()
- // .bindPopup('Hello world!')
-    // .on('click', function() { alert('Clicked on a member of the group!'); })
-	;  
-
-var overlays = {
-	// "ВКЛ, 1430 гг.": vkl,
-	"test": testlayer
-};
-
-
-var labelTextCollision = new L.LabelTextCollision({
-                            collisionFlg : true
-                        });
-  
-  
-
-var map = L.map('map', { 
-	zoomControl:false, 
-	center: [53.916667, 27.55],
-	zoom: 7, 
-	// layers: [wikimedia],
-	// layers: [datamap, vkl],
-	layers: [datamap, testlayer],
-	renderer : labelTextCollision
-});
-  
-
-// map.createPane('borders');
-// map.getPane('borders').style.zIndex = 650;
-// map.getPane('borders').style.pointerEvents = 'none';
-
-// map.createPane('cities');
-// map.getPane('cities').style.zIndex = 1050;
-// map.getPane('cities').style.pointerEvents = 'all';
-
-L.control.layers(Object.assign(baseLayers2), overlays).addTo(map);
-// L.marker([53.916667, 27.55], {icon: redMarker}).bindPopup('Мінск').addTo(map);
-
-var svg = '<svg height="210" width="500"><polygon points="100,10 40,198 190,78 10,78 160,198" style="fill:lime;stroke:purple;stroke-width:5;fill-rule:nonzero;"/></svg>';
-var iconUrl = 'data:image/svg+xml;base64,' + btoa(svg);
-
-var icon = L.icon( {
-            // iconUrl: 'mdbg.svg',
-            iconUrl: iconUrl,
-        } );
-
- var CustomIcon = L.Icon.extend({
-			options: {
-				iconSize:     [40, 40],
-				// shadowSize:   [50, 64],
-				// iconAnchor:   [22, 94],
-				// shadowAnchor: [4, 62],
-				// popupAnchor:  [-3, -76]
-			}
-		});
-// https://leafletjs.com/reference-1.6.0.html#icon    
-// var url = encodeURI("data:image/svg+xml," + svgrect).replace('#','%23');
-// console.log(url);
-
-var MagdIcon = new CustomIcon({iconUrl: '/mdbg.svg'});
-// var nIcon = L.divIcon({ html: mgdsvg, iconSize: [24, 38], iconAnchor: [12, 38] })
-		
-// L.marker([53.56807, 26.13735], {icon: MagdIcon}).bindPopup('Камень').addTo(testlayer);
-L.marker([53.56807, 26.13735], {icon: MagdIcon})
-	// .bindLabel('123', {
-		// noHide: true,
-		// direction: 'auto'
-	// })
-	.bindTooltip("Карэлічы", {permanent: true, className: "cityname", offset: [0, -20], opacity: 1, direction: "center" })
-	.bindPopup('Скарб').addTo(testlayer);
-
-		// d3.json("vkl1430.geojson", function(error, data) {
-				// var geojsonFeature = data.features[0];
-				// // console.log(geojsonFeature);
-				
-				// L.geoJSON(geojsonFeature, {interactive: false, pane: 'borders'}).addTo(vkl);
-				// vkl.setStyle({className: 'vkl'});
-	
-				
-
-			// });
+	var west = L.tileLayer('https://tiles.historic.place/region/Westrussland/{z}/{x}/{y}.png', { maxZoom: 11, minZoom: 8, opacity: 1} );
 	
 
+	var baseLayers2 = {			
 
-	/* Initialize the SVG layer */
-	// var svgLayer = L.svg({attribution: "Gardariki", className: 'dots', pane: 'cities'});
-	var svgLayer = L.svg();
-	svgLayer.addTo(map); 
-	var tip;
+		"Datamap":  datamap,
+		"OSM":  L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 1} ),
+		"OSM.DE":  L.tileLayer("https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png", { maxZoom: 19, minZoom: 4, opacity: 1} ),
+		"AWMC":  L.tileLayer("https://a.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png", { maxZoom: 19, minZoom: 6, opacity: 1} ),
+		//positron
+		"CartoDB":  L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", { maxZoom: 19, minZoom: 6, opacity: 1, "attribution":
+		  "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://cartodb.com/attributions'>CartoDB</a>",
+		"subdomains": "abcd"} ),
+		"OSM (no lbl)":  L.tileLayer('https://tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 1} ),
+		"Watercolor":  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', { maxZoom: 19, minZoom: 6, opacity: 1} ),
+		"River":  L.tileLayer('https://{s}.tile.openstreetmap.fr/openriverboatmap/{z}/{x}/{y}.png', { maxZoom: 19, minZoom: 6, opacity: 1} ),
+		//'',
+		// ,
+		"Wikimedia": wikimedia,
+		"USGS": natmap,
+		"ESRI Satellite": L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'	, { attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, 	IGP, UPR-EGP, and the GIS User Community'}),
+		"Google Satellite" : L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+			maxZoom: 20,
+			subdomains:['mt0','mt1','mt2','mt3'],
+			attribution: "Google Maps"
+		})	
+	};
 
-	/* We simply pick up the SVG from the map object */
-		var svg = d3.select("#map").select("svg");
-		
-		svg.style("pointer-events", "all"); 
-		
-		// svg.raise();
-		// console.log("raise");
-		
-		var g = d3.select("#map").select("svg").select('g');
-		g
-		// .attr("class", "leaflet-zoom-hide")
-		 // .style("position", "absolute")
-    // .style("background", "white")
-    // .style("opacity", "0")
-    // .style("padding", "0 10px")
-    // .style("z-index", "999");
-	;
-		
-tip = d3.tip().attr('class', 'd3-tip').html(function(d) { 
-		// console.log(d.properties.name);
-		return d.properties.name; 
-		});
-		svg.call(tip); 
-		
-		
-		
-		
+	var datalayer = L.featureGroup()
+	 // .bindPopup('Hello world!')
+		// .on('click', function(e) { 
+			// alert('Clicked on a member of the group!'); 
+			// // console.log(e);
+			// console.log(this.options);
+			// console.log(e);
+		// })
+		;  
+
+	var overlays = {
+		"Гарады": datalayer,
+		"1905": west
+	};
+
+	var map = L.map('map', { 
+		zoomControl:false, 
+		center: [53.916667, 27.55],
+		zoom: 7, 
+		// layers: [wikimedia],
+		// layers: [datamap, vkl],
+		layers: [datamap, datalayer],
+	});
+	  
+	L.control.layers(Object.assign(baseLayers2), overlays).addTo(map);
+	
+		var redMarker = L.VectorMarkers.icon({
+		//icon: 'exclamation-triangle',
+		icon: 'landmark',
+		markerColor:"white",
+		iconColor: "black",
+		extraClasses: 'fas',
+		prefix: 'fa'
+		// spin: true,
+	  });
+	  
+	  
+	// L.marker([53.916667, 27.55], {icon: redMarker}).bindPopup('Мінск').addTo(map);
+
+
+	 var CustomIcon = L.Icon.extend({
+				options: {
+					iconSize:     [40, 40],
+					// shadowSize:   [50, 64],
+					// iconAnchor:   [22, 94],
+					// shadowAnchor: [4, 62],
+					// popupAnchor:  [-3, -76]
+				}
+			});
+
+	var MagdIcon = new CustomIcon({iconUrl: '/mdbg.svg'});
+	// var nIcon = L.divIcon({ html: mgdsvg, iconSize: [24, 38], iconAnchor: [12, 38] })
+			
+			
+	var onMarkerClick = function(e){
+		var cid = this.options.city;
+		var cll = e.latlng;
+		console.log(cid, e.latlng);
+		// return;
+		d3.text("/api?id="+cid, function(result) {
+			map.openModal({content: result});
+			$('.tabs').tabs();
+			$('.click').click(function(){
+				// map.panTo(new L.LatLng(52, 24));
+				console.log(cid);
+				map.setView(cll, 14);
+				map.closeModal();
+				if (pois.hasOwnProperty(cid)){
+					// pois[cid]
+				} else {
+					d3.json("data/poi/"+cid+'.json', function(info) {
+							pois[cid] = info;
+							
+							for (x of info){
+									console.log(x.tourism);
+									// console.log(x);
+									// console.log(x.geometry.coordinates);
+									// L.marker([x.geometry.coordinates[1], x.geometry.coordinates[0]], {icon: redMarker}).bindPopup('Мінск').addTo(vkl);
+									L.marker([x.geometry.coordinates[1], x.geometry.coordinates[0]], {icon: redMarker}).addTo(map).on('mouseover', function(e){console.log("kek");});
+							}
+					});							
+				}
+			});
+		});		
+	}		
+	// L.marker([53.56807, 26.13735], {icon: MagdIcon}).bindPopup('Камень').addTo(datalayer);
+	// L.marker([53.56807, 26.13735], {"icon": MagdIcon, "city": 100});
 
 	d3.json("list.json", function(geo) {
-		/* Add a LatLng object to each item in the dataset */
-		// console.log(htls);
-		
-		
-		
-		
-		
-		var datum = geo.features;
-		datum.forEach(function(d) {
-			
-			if (d.hasOwnProperty("geometry")){
-				var ll = d["geometry"]["coordinates"];
-				d.LatLng = new L.LatLng(ll[1],ll[0]);	
-				// console.log(d.LatLng);
+		geo.features.forEach(function(d) {
+			if (d.properties.id != 6) {
+			var ll = d["geometry"]["coordinates"];
+			d.LatLng = new L.LatLng(ll[1],ll[0]);	
+			// var c = L.circleMarker(d.LatLng, { radius : 5, text : d.properties.name});
+			var place =  L.marker(d.LatLng, {"icon": MagdIcon, "city": d.properties.id})
+				.on('click', onMarkerClick)
+				// .bindLabel('123', { noHide: true, direction: 'auto'})
+				.bindTooltip(d.properties.name, {permanent: true, className: "cityname", offset: [0, -20], opacity: 1, direction: "center" })
+				// .bindPopup('Скарб')
+				.addTo(datalayer)
+				;
 			}
-				
-				// console.log(d);
-                        var c = L.circleMarker(d.LatLng, {
-                            radius : 5,
-                            text : d.properties.name
-                        });
-                        testlayer.addLayer(c);
-			
 		})
-		// var domainOnlyScale = d3.scale.linear().domain([0,2]);
-		
-		var colors = d3.scaleQuantize()
-		.domain([0,2])
-		.range(['#f1eef6','#d7b5d8','#df65b0','#dd1c77','#980043']);
-		
-		var cls  = ['#c994c7','#df65b0','#e7298a','#ce1256','#91003f'];
-	
-	
-		
-	
-		var feature = g.selectAll("circle")
-			.data(datum)
-			.enter().append("circle")
-			.style("stroke", "pink")  
-			// .style("opacity", .6) 
-			// .style("fill", "red")
-			.style("fill", function(d){
-
-				return "darkred";
-			})
-			.attr("r", 10)
-			.on('mouseover', tip.show)
-			.on('mouseout', tip.hide)
-			.on('click' , function(d){ 
-				// console.log(d.properties.wiki);
-				// d3.json("/city?id="+d.properties.wiki, function(datum) {
-				d3.text("/api?id="+d.properties.id, function(result) {
-					map.openModal({content: result});
-					$('.tabs').tabs();
-					$('.click').click(function(){
-						// map.panTo(new L.LatLng(52, 24));
-						console.log(d.properties.id);
-						map.setView(new L.LatLng(d.geometry.coordinates[1], d.geometry.coordinates[0]), 14);
-						map.closeModal();
-						if (pois.hasOwnProperty(d.properties.id)){
-							// pois[d.properties.id]
-						} else {
-							d3.json("data/poi/"+d.properties.id+'.json', function(info) {
-									pois[d.properties.id] = info;
-									
-									for (x of info){
-										// console.log(x.tourism);
-											// console.log(x);
-											// console.log(x.geometry.coordinates);
-											// L.marker([x.geometry.coordinates[1], x.geometry.coordinates[0]], {icon: redMarker}).bindPopup('Мінск').addTo(vkl);
-											L.marker([x.geometry.coordinates[1], x.geometry.coordinates[0]], {icon: redMarker}).addTo(map).on('mouseover', function(e){console.log("kek");});
-									}
-							});							
-						}
-						
-					});
-				});
-
-			})
-			;  
-		map.on("viewreset", update);
-		map.on("zoomend", update);
-		
-		
-		
-		
-		update();
-		function update() {
-			// console.log("update zoom");
-						
-			feature.attr("transform", 
-			function(d) { 
-				return "translate("+ 
-					map.latLngToLayerPoint(d.LatLng).x +","+ 
-					map.latLngToLayerPoint(d.LatLng).y +")";
-				}
-			)
-		}
 	})			 
-
-// var customControl =  L.Control.extend({
-  // options: {
-	// position: 'topleft'
-  // },
-  // onAdd: function (map) {
-	// var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom info');
-	// container.innerHTML = map.getZoom();
-	// return container;
-  // },
-  // update: function (props) {	
-	// this._container.innerHTML = props;
-	// }
-// });
-
-// var info = new customControl();
-// map.addControl(info);
-// map.on('baselayerchange', function (e) {
-	// info.update(e.name);
-// });
-// map.on('overlayadd', function (e) {
-// console.log(e.layer.bringToBack());	
-// }); 
-
-// var layerControlElement = document.getElementsByClassName('leaflet-control-layers')[0];
+});
