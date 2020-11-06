75f611f84a24c22c4ffd517ddb1054dd75f67309
yaskevich <alyaxey.yaskevich@gmail.com>
2020-11-06 21:26:14 +0300

    update CLI

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gardariki
+
+Frontend application which provides data from API to client
--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const axios = require('axios');
 const fs = require('fs');
 const pr = require('./processing');
 const cfg = pr.init();
@@ -7,17 +8,32 @@ if (process.argv.length > 2 && process.argv[2] == "cron") {
 	pr.logger.level = "warn";	
 }
 
-pr.getAllCities().then(function(result) {
-	const items = result.data;
-	const ids = items
-		.map((x) => cfg.front.languages
-			.map((l) => pr.generateCityModal(l, x.id, true)
-				.then(() => pr.logger.info(`done ${l} ${x.id}`) )
-				)
-			);
-	Promise.all(ids.flat())
-		.then(() => pr.logger.info(`modals done [${items.length}]`) )
-	;
-	// console.log(`generated   items`);
-});
-pr.generateListOfCities (true).then(() => pr.logger.info("list done"));
+
+axios.get('http://localhost:4141')
+    .then(resp => {
+        pr.logger.info(`Backend status ${resp.status}`);
+		if (resp.status === 200){
+			pr.logger.info("Querying backend...");
+			pr.getAllCities().then(function(result) {
+				const items = result.data;
+				const ids = items
+					.map((x) => cfg.front.languages
+						.map((l) => pr.generateCityModal(l, x.id, true)
+							.then(() => pr.logger.info(`done ${l} ${x.id}`) )
+							)
+						);
+				Promise.all(ids.flat())
+					.then(() => pr.logger.info(`modals done [${items.length}]`) )
+				;			
+			});	
+			pr.generateListOfCities (true).then(() => pr.logger.info("list done"));			
+		}
+    })
+    .catch(err => {
+       pr.logger.error("Backend is not available!");
+    });
+
+
+
+
+
