9305dad30c1c02d92017df1a0885f8f386a87e74
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-09-16 14:23:54 +0200

    rewrite with args

--- cli.js
+++ cli.js
@@ -1,39 +1,23 @@
-const path = require('path');
-const axios = require('axios');
-const fs = require('fs');
-const pr = require('./processing');
-const cfg = pr.init();
+import args from 'args';
+import pr from './processing.js';
 
-if (process.argv.length > 2 && process.argv[2] == "cron") {
-	pr.logger.level = "warn";	
+args // (e.g. when running under cron)
+  .option('quiet', 'Show only important warnings', false)
+  .option('static', 'Generate static cache', false)
+  .option('inline', 'Generate inline HTML of the map', false);
+
+const flags = args.parse(process.argv);
+// console.log(flags);
+
+if (flags.quiet) {
+  pr.logger.level = 'warn';
 }
 
+if (flags.static) {
+  pr.buildAllModals();
+}
 
-axios.get('http://localhost:4141')
-    .then(resp => {
-        pr.logger.info(`Backend status ${resp.status}`);
-		if (resp.status === 200){
-			pr.logger.info("Querying backend...");
-			pr.getAllCities().then(function(result) {
-				const items = result.data;
-				const ids = items
-					.map((x) => cfg.front.languages
-						.map((l) => pr.generateCityModal(l, x.id, true)
-							.then(() => pr.logger.info(`done ${l} ${x.id}`) )
-							)
-						);
-				Promise.all(ids.flat())
-					.then(() => pr.logger.info(`modals done [${items.length}]`) )
-				;			
-			});	
-			pr.generateListOfCities (true).then(() => pr.logger.info("list done"));			
-		}
-    })
-    .catch(err => {
-       pr.logger.error("Backend is not available!");
-    });
-
-
-
-
-
+// server should be online
+if (flags.inline) {
+  await pr.buildInlineMap();
+}
