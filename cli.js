bbd7b7f670422ecd98abbf54e60f1148af4d7cd8
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-11-07 13:12:58 +0100

    add fetch option to CLI

--- cli.js
+++ cli.js
@@ -4,7 +4,8 @@ import pr from './processing.js';
 args // (e.g. when running under cron)
   .option('quiet', 'Show only important warnings', false)
   .option('static', 'Generate static cache', false)
-  .option('inline', 'Generate inline HTML of the map', false);
+  .option('inline', 'Generate inline HTML of the map', false)
+  .option('fetch', 'Fetch Magdeburg Law data', false);
 
 const flags = args.parse(process.argv);
 // console.log(flags);
@@ -20,4 +21,8 @@ if (flags.static) {
 // server should be online
 if (flags.inline) {
   await pr.buildInlineMap();
+}
+
+if (flags.fetch) {
+  await pr.getMagdeburg(flags.quiet);
 }
